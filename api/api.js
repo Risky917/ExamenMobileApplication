7671f5a336f5b794aea683f723c991d80488700f
Mathias <mathiasvanneste713@gmail.com>
2025-11-05 11:30:07 +0100

    api.js: Added functions to fetch products and product details

--- api/api.js
+++ api/api.js
@@ -1,1 +1,40 @@
 // api
+const BASE_URL = 'https://dummyjson.com/products';
+
+export const fetchProducts = () => {
+  console.log('fetchProducts gestart...');
+  return fetch(BASE_URL)
+    .then((res) => {
+      if (!res.ok) {
+        throw Error('API call mislukt: ' + res.status);
+      }
+      return res.json();
+    })
+    .then((data) => {
+      console.log('fetchProducts success');
+      return data.products;
+    })
+    .catch((err) => {
+      console.error('fetchProducts error:', err);
+      throw err;
+    });
+};
+
+export const fetchProductDetail = (id) => {
+  console.log(`fetchProductDetail gestart voor id ${id}...`);
+  return fetch(`${BASE_URL}/${id}`)
+    .then((res) => {
+      if (!res.ok) {
+        throw Error('Product niet gevonden');
+      }
+      return res.json();
+    })
+    .then((data) => {
+      console.log('fetchProductDetail success');
+      return data;
+    })
+    .catch((err) => {
+      console.error('fetchProductDetail error:', err);
+      throw err;
+    });
+};
